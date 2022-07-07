99537cc24eb4a0ae045aab373830d715af6eb82a
Lukasz Olszak <lukasz.olszak@volvocars.com>
2022-07-07 16:38:27 +0200

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # this is your part to fill
+
+# Deploy invoice-app
+echo -n "Deploy invoice-app -> " && kubectl apply -f invoice-app/deployment.yaml
+
+# Deploy payment-provider
+echo -n "Deploy payment-provider -> " && kubectl apply -f payment-provider/deployment.yaml
